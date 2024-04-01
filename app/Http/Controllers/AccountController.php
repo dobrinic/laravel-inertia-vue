34d585c7258fb53aea29d9771b68b13866f9d95e
Aleksandar Dobrinic <dobrinich@gmail.com>
2024-04-01 18:40:30 +0200

    Added Controllers Task

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,41 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Inertia\Inertia;
+use Inertia\Response as InertiaResponse;
 
 class AccountController extends Controller
 {
-    public function index()
+    public function index(): InertiaResponse
     {
-
+        $accounts = Account::all();
+        
+        return Inertia::render('Accounts/Index', compact('accounts'));
     }
 
-    public function show()
+    public function show(Account $account): InertiaResponse
     {
+        // could transform account and contacts to a DTO or Resource Class to not expose unneccessary data to the Client
+        $account->load(['contacts', 'owner' => function ($query) {
+                $query->select('id', 'name');
+            }
+        ]);
 
+        return Inertia::render('Accounts/Show', compact('account'));
     }
 
-    public function create()
+    public function create(): InertiaResponse
     {
-
+        $owners = User::select('id', 'name')->get();
+        
+        return Inertia::render('Accounts/Create', compact('owners'));
     }
 
-    public function store()
+    public function store(Request $request): RedirectResponse
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'town_city' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'post_code' => 'required|string|max:255',
+            'phone' => 'required|string|max:255',
+            'owner_id' => ['required', Rule::exists('users', 'id')]
+        ]);
 
+        Account::create($validated);
+
+        return redirect()->route('accounts.index');
     }
 
-    public function edit()
+    public function edit(Account $account)
     {
+        $owners = User::select('id', 'name')->get();
 
+        return Inertia::render('Accounts/Edit', compact('account', 'owners'));
     }
 
-    public function update()
+    public function update(Request $request, Account $account)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'town_city' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'post_code' => 'required|string|max:255',
+            'phone' => 'required|string|max:255',
+            'owner_id' => ['required', Rule::exists('users', 'id')]
+        ]);
 
+        $account->update($validated);
+
+        return redirect()->route('accounts.show', $account);
     }
 
-    public function destroy()
+    public function destroy(Account $account)
     {
+        $account->delete();
 
+        return redirect()->route('accounts.index');
     }
 }
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,14 @@ class Contact extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'position'];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => $attributes['first_name'] . ' ' . $attributes['last_name'],
+        );
+    }
 
     public function account()
     {
